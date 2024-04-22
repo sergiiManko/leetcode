38a3c0c5b4cd978fdb232c2352dfff14b91f6905
Serhii Manko <sergii.manko@gmail.com>
2024-04-22 17:26:49 +0200

    Solution for `Sorted squares`

---
+++ src/main/java/com/leetcode/SortedSquares.java
@@ -0,0 +1,40 @@
+package com.leetcode;
+
+/**
+ * @author Serhii Manko
+ * Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [-4,-1,0,3,10]
+ * Output: [0,1,9,16,100]
+ * Explanation: After squaring, the array becomes [16,1,0,9,100].
+ * After sorting, it becomes [0,1,9,16,100].
+ * Example 2:
+ * <p>
+ * Input: nums = [-7,-3,2,3,11]
+ * Output: [4,9,9,49,121]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums is sorted in non-decreasing order.
+ */
+public class SortedSquares {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int i = 0, j = nums.length - 1;
+        for (int p = nums.length - 1; p >= 0; p--) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                result[p] = nums[i] * nums[i];
+                i++;
+            } else {
+                result[p] = nums[j] * nums[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}
